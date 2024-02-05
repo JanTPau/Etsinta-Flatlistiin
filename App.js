6bb9a3c1f9ea6a0b8355d9f7bfc4b6fc4c1921bd
JanTPau <127750707+JanTPau@users.noreply.github.com>
2024-02-05 18:17:59 +0200

    Search
    Search in flatlist assignment

--- App.js
+++ App.js
@@ -2,12 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
 import {DATA} from './Data'
 import Row from './components/Row';
+import Search from './components/Search';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(DATA);
+  }, [])
+
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter ((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch} />
       <FlatList
-        data={DATA}
+        data={items}
         renderItem = {({item}) =>(
           <Row person= {item} />
         )}
